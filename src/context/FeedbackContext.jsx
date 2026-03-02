0fa352be5305256df9ec3df3f3e804275bd881b3
Fidelis Chizoba <chyzee@Fideliss-MacBook-Pro.local>
2026-03-02 16:57:28 +0100

    test

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -16,7 +16,9 @@ export const FeedbackProvider = ({ children }) => {
 
     // fetch feedback
     const fetchFeedback = async () => {
-      const response = await fetch("http://localhost:3000/feedback?_sort=id&_order=desc")
+      const response = await fetch(
+        "https://chyzee-feedbackapp.netlify.app?_sort=id&_order=desc",
+      );
       const data = await response.json()
 
       setFeedback(data)
@@ -25,13 +27,13 @@ export const FeedbackProvider = ({ children }) => {
 
     // add feedback
     const addFeedback = async(newFeedback) => {
-      const response = await fetch("http://localhost:3000/feedback", {
+      const response = await fetch("https://chyzee-feedbackapp.netlify.app", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(newFeedback),
-      })
+      });
         const data = await response.json()
 
         setFeedback([data,...feedback])
@@ -40,7 +42,7 @@ export const FeedbackProvider = ({ children }) => {
     //   delete feedback
     const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
-           await fetch(`http://localhost:3000/feedback/${id}`, {
+           await fetch(`https://chyzee-feedbackapp.netlify.app${id}`, {
             method: "DELETE",
           })
 
@@ -50,13 +52,16 @@ export const FeedbackProvider = ({ children }) => {
 
       //   update feedback item
       const updateFeedback = async (id, updItem) => {
-        const response = await fetch(`http://localhost:3000/feedback/${id}`, {
+        const response = await fetch(
+          `https://chyzee-feedbackapp.netlify.app${id}`,
+          {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
             },
             body: JSON.stringify(updItem),
-          })
+          },
+        );
           const data = await response.json()
 
         setFeedback(feedback.map((item) => item.id === id? {...item, ...data} :item))
